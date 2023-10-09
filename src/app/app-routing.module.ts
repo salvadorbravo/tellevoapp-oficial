ae0bfadd2afb13067332d32196b3c01b1d7a55d4
Salvador Bravo <sa.bravo@duocuc.cl>
2023-10-09 20:44:09 -0300

    primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { NoAutenticadoGuard } from './guards/no-autenticado.guard';
+import { AutenticacionGuard } from './guards/autenticacion.guard';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: '',
+    redirectTo: 'autenticacion',
+    pathMatch: 'full'
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: 'autenticacion',
+    loadChildren: () => import('./paginas/autenticacion/autenticacion.module').then(m => m.AutenticacionPageModule), canActivate: [NoAutenticadoGuard]
+  },
+  {
+    path: 'main',
+    loadChildren: () => import('./paginas/main/main.module').then(m => m.MainPageModule), canActivate: [AutenticacionGuard]
   },
 ];
 
